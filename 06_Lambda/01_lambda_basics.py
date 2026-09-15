d290aad8588785cef1d8365f9a01ac658b312f70
Anjali Ramesh Sangannavar <sangannavaranjali@gmail.com>
2026-09-15 18:25:04 +0530

    Create a new folder called 06_Lambda to add the lambda related practiced work

---
+++ 06_Lambda/01_lambda_basics.py
@@ -0,0 +1,23 @@
+# Q1: Square of a number
+square = lambda num: num * num
+print("Square:", square(2))
+
+
+# Q2: Multiplication of two numbers
+multi = lambda num1, num2: num1 * num2
+print("Multiplication:", multi(2, 3))
+
+
+# Q3: Subtraction of two numbers
+sub = lambda num1, num2: num1 - num2
+print("Subtraction:", sub(5, 2))
+
+
+# Q4: Division of two numbers
+divide = lambda num1, num2: num1 / num2
+print("Division:", divide(10, 5))
+
+
+# Q5: Double a number
+double = lambda num: num + num
+print("Double:", double(8))
